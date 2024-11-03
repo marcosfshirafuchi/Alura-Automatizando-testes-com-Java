8d05754a6f84bb119616f4f888e228d0737ca44d
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2024-11-03 19:09:30 -0300

    Criação dos teste unitários da classe: AdocaoControllerTest

--- src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -1,18 +1,23 @@
 package br.com.alura.adopet.api.controller;
 
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.service.AdocaoService;
-import org.junit.jupiter.api.Assertions;
+import jakarta.validation.ValidationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.awt.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 //Essa anotação vai ajudar a fazer a injeção de indepencia na classe de test
 @SpringBootTest
@@ -49,7 +54,7 @@ class AdocaoControllerTest {
 
         //ASSERT
         //Esperado 400 no primeiro campo e retornar o estado do response no segundo campo
-        Assertions.assertEquals(400, response.getStatus());
+        assertEquals(400, response.getStatus());
     }
 
     @Test
@@ -78,6 +83,112 @@ class AdocaoControllerTest {
 
         //ASSERT
         //Esperado 200 no primeiro campo e retornar o estado do response no segundo campo
-        Assertions.assertEquals(200, response.getStatus());
+        assertEquals(200, response.getStatus());
+    }
+
+    @Test
+    void deveriaDevolverCodigo200ParaRequisicaoDeAprovarAdocao() throws Exception{
+        //ARRANGE
+        String json = """
+                {
+                    "idAdocao":1
+                }
+                """;
+
+        //ACT
+        MockHttpServletResponse response = mvc.perform(
+                put("/adocoes/aprovar")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //ASSERT
+        assertEquals(200, response.getStatus());
+    }
+
+    @Test
+    void deveriaDevolverCodigo400ParaRequisicaoDeAprovarAdocaoInvalida() throws Exception{
+        //AARANGE
+        String json = """
+                {
+                
+                }
+                """;
+
+        //ACT
+        MockHttpServletResponse response = mvc.perform(
+                put("/adocoes/aprovar")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //ASSERT
+        assertEquals(400, response.getStatus());
+    }
+
+    @Test
+    void deveriaDevolverCodigo200ParaRequisicaoDeReprovarAdocao() throws Exception{
+        //ARRANGE
+        String json = """
+                {
+                    "idAdocao":1,
+                    "justificativa": "qualquer"
+                }
+                """;
+
+        //ACT
+        MockHttpServletResponse response = mvc.perform(
+                put("/adocoes/reprovar")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //ASSERT
+        assertEquals(200,response.getStatus());
+    }
+
+    @Test
+    void deveriaDevolverCodigo400ParaRequisicaoDeReprovarAdocaoInvalida() throws Exception{
+        //ARRANGE
+        String json = """
+                {
+                
+                }
+                """;
+
+        //ACT
+        MockHttpServletResponse response = mvc.perform(
+                put("/adocoes/reprovar")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //ASSERT
+        assertEquals(400,response.getStatus());
+    }
+
+    @Test
+    void deveriaDevolverCodigo400QuandoExceptionForLancadaNoSolicitarAdocao() throws Exception {
+        // ARRANGE
+        String json = """
+                {
+                    "idPet": 1,
+                    "idTutor": 1,
+                    "motivo": "Motivo qualquer"
+                }
+                """;
+
+        // Simulando o comportamento do service lançando uma exceção de validação
+        doThrow(new ValidationException("Erro na validação da adoção"))
+                .when(service).solicitar(any(SolicitacaoAdocaoDto.class));
+
+        // ACT & ASSERT
+        mvc.perform(
+                        post("/adocoes")
+                                .content(json)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("Erro na validação da adoção"));
     }
 }
